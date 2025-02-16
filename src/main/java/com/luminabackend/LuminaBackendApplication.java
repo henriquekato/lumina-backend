c63a86b8dc935c1aa256c74476b0eba2840b2fca
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-02-16 14:05:18 -0300

    Project creation

---
+++ src/main/java/com/luminabackend/LuminaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.luminabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuminaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luminabackend/LuminaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luminabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
