cf44c63acd9aff550ea13790e66002a2434d5aaa
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-02-26 18:17:07 -0300

    chore: remove obsolete classes

--- src/main/java/com/luminabackend/utils/security/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.luminabackend.utils.security;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
