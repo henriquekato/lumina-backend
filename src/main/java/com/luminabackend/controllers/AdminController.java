a505429c149657253e21b759b8e27660559eb934
Shoji <hshojikato@gmail.com>
2025-03-09 10:21:50 -0300

    feat: add documention for admin controller endpoints

--- src/main/java/com/luminabackend/controllers/AdminController.java
+++ src/main/java/com/luminabackend/controllers/AdminController.java
@@ -6,6 +6,13 @@ import com.luminabackend.models.user.dto.admin.AdminGetDTO;
 import com.luminabackend.models.user.dto.user.UserPutDTO;
 import com.luminabackend.models.user.dto.user.UserSignupDTO;
 import com.luminabackend.services.AdminService;
+import com.luminabackend.utils.errors.GeneralErrorResponseDTO;
+import com.luminabackend.utils.errors.ValidationErrorResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +23,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@ApiResponses(value = {
+        @ApiResponse(
+                responseCode = "401",
+                description = "Unauthorized. Incorrect or invalid credentials",
+                content = { @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+})
 @RestController
 @PreAuthorize("hasRole('ADMIN')")
 @RequestMapping("/admin")
@@ -23,12 +38,35 @@ public class AdminController {
     @Autowired
     private AdminService service;
 
+    @Operation(summary = "Get a list of admins")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Returns a list of admins",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AdminGetDTO.class)) })
+    })
     @GetMapping
     public ResponseEntity<List<AdminGetDTO>> getAllAdmins() {
         List<Admin> admins = service.getAllAdmins();
         return ResponseEntity.ok(admins.stream().map(AdminGetDTO::new).toList());
     }
 
+    @Operation(summary = "Get a admin by its id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Returns the specified admin",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AdminGetDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Admin not found",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<AdminGetDTO> getAdmin(@PathVariable UUID id) {
         Optional<Admin> adminById = service.getAdminById(id);
@@ -37,12 +75,60 @@ public class AdminController {
                 .orElseThrow(() -> new EntityNotFoundException("Admin not found"));
     }
 
+    @Operation(summary = "Create a new admin")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Returns the created admin",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AdminGetDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Validation errors",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ValidationErrorResponseDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Email already in use",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+    })
     @PostMapping
     public ResponseEntity<AdminGetDTO> saveAdmin(@Valid @RequestBody UserSignupDTO adminPostDTO) {
         Admin newAdmin = service.save(adminPostDTO);
         return ResponseEntity.ok(new AdminGetDTO(newAdmin));
     }
 
+    @Operation(summary = "Edit a admin by its id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Returns the edited admin",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AdminGetDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Validation errors",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ValidationErrorResponseDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Admin not found",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Email already in use",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+    })
     @PutMapping("/{id}")
     public ResponseEntity<AdminGetDTO> editAdmin(
             @PathVariable UUID id,
@@ -51,6 +137,18 @@ public class AdminController {
         return ResponseEntity.ok(new AdminGetDTO(admin));
     }
 
+    @Operation(summary = "Delete a admin by its id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Successfully delete the admin"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Admin not found",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteAdmin(@PathVariable UUID id) {
         service.deleteById(id);
