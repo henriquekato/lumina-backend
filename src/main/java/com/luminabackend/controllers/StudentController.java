8dd57ef9085c0a473aac1aa3e4ca4bab6937da37
Shoji <hshojikato@gmail.com>
2025-03-09 10:22:05 -0300

    feat: add documention for student controller endpoints

--- src/main/java/com/luminabackend/controllers/StudentController.java
+++ src/main/java/com/luminabackend/controllers/StudentController.java
@@ -6,6 +6,13 @@ import com.luminabackend.models.user.dto.student.StudentGetDTO;
 import com.luminabackend.models.user.dto.user.UserPutDTO;
 import com.luminabackend.models.user.dto.user.UserSignupDTO;
 import com.luminabackend.services.StudentService;
+import com.luminabackend.utils.errors.GeneralErrorResponseDTO;
+import com.luminabackend.utils.errors.ValidationErrorResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +23,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@ApiResponses(value = {
+        @ApiResponse(
+                responseCode = "401",
+                description = "Unauthorized. Incorrect or invalid credentials",
+                content = { @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+})
 @RestController
 @PreAuthorize("hasRole('ADMIN')")
 @RequestMapping("/student")
@@ -23,12 +38,35 @@ public class StudentController {
     @Autowired
     private StudentService service;
 
+    @Operation(summary = "Get a list of students")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Returns a list of students",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StudentGetDTO.class)) })
+    })
     @GetMapping
     public ResponseEntity<List<StudentGetDTO>> getAllStudents() {
         List<Student> students = service.getAllStudents();
         return ResponseEntity.ok(students.stream().map(StudentGetDTO::new).toList());
     }
 
+    @Operation(summary = "Get a student by its id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Returns the specified student",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StudentGetDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Student not found",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<StudentGetDTO> getStudent(@PathVariable UUID id) {
         Optional<Student> studentById = service.getStudentById(id);
@@ -37,12 +75,60 @@ public class StudentController {
                 .orElseThrow(() -> new EntityNotFoundException("Student not found"));
     }
 
+    @Operation(summary = "Create a new student")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Returns the created student",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StudentGetDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Validation errors",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ValidationErrorResponseDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Email already in use",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+    })
     @PostMapping
     public ResponseEntity<StudentGetDTO> saveStudent(@Valid @RequestBody UserSignupDTO studentPostDTO) {
         Student newStudent = service.save(studentPostDTO);
         return ResponseEntity.ok(new StudentGetDTO(newStudent));
     }
 
+    @Operation(summary = "Edit a student by its id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Returns the edited student",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StudentGetDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Validation errors",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ValidationErrorResponseDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Student not found",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Email already in use",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+    })
     @PutMapping("/{id}")
     public ResponseEntity<StudentGetDTO> editStudent(
             @PathVariable UUID id,
@@ -51,6 +137,18 @@ public class StudentController {
         return ResponseEntity.ok(new StudentGetDTO(student));
     }
 
+    @Operation(summary = "Delete a student by its id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Successfully delete the student"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Student not found",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteStudent(@PathVariable UUID id) {
         service.deleteById(id);
