ecaf3bfe36913925ead4776faa91ec620d78ae13
Shoji <hshojikato@gmail.com>
2025-03-09 11:17:03 -0300

    feat: add documention for login endpoint

--- src/main/java/com/luminabackend/controllers/AccountController.java
+++ src/main/java/com/luminabackend/controllers/AccountController.java
@@ -3,7 +3,12 @@ package com.luminabackend.controllers;
 import com.luminabackend.models.user.User;
 import com.luminabackend.models.user.dto.user.UserLoginDTO;
 import com.luminabackend.services.TokenService;
+import com.luminabackend.utils.errors.GeneralErrorResponseDTO;
 import com.luminabackend.utils.security.TokenDTO;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +28,20 @@ public class AccountController {
     @Autowired
     private TokenService tokenService;
 
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully login",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TokenDTO.class)) }),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized. Incorrect or invalid credentials",
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponseDTO.class)) }),
+    })
     @PostMapping("/login")
     public ResponseEntity<TokenDTO> login(@Valid @RequestBody UserLoginDTO data){
         String email = data.email().trim();
