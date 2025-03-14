d2964c5dd21a26e4ccc475c80bafacf150966720
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-03-14 10:13:29 -0300

    feat: add ZipProcessingException

---
+++ src/main/java/com/luminabackend/exceptions/ZipProcessingException.java
@@ -0,0 +1,7 @@
+package com.luminabackend.exceptions;
+
+public class ZipProcessingException extends RuntimeException {
+    public ZipProcessingException(String message) {
+        super(message);
+    }
+}
